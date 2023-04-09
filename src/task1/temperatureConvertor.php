b4daa6ac1a3c0fa0583973ae97b0dc054a268dad
shree0007 <shree.bhusal07@gmail.com>
2023-04-09 20:48:51 +0300

    added temperature converter part

--- src/task1/temperatureConvertor.php
+++ src/task1/temperatureConvertor.php
@@ -5,7 +5,23 @@ class temperatureConvertor
 {
     public function convert(string $first, string $second, float $amount): float
     {
-        return 0;
+
+        switch (true) {
+            case ($first == 'celsius' && $second == 'fahrenheit'):
+                return round ($amount * 9/5) + 32;
+            case ($first == 'celsius' && $second == 'kelvin'):
+                return round ($amount + 273.15);
+            case ($first == 'fahrenheit' && $second == 'celsius'):
+                return round ($amount - 32) * 5/9;
+            case ($first == 'fahrenheit' && $second == 'kelvin'):
+                return round ($amount + 459.67) * 5/9;
+            case ($first == 'kelvin' && $second == 'celsius'):
+                return round ($amount - 273.15);
+            case ($first == 'kelvin' && $second == 'fahrenheit'):
+                return round (($amount * 9/5) - 459.67);
+            default:
+                die(`You have chosen the wrong units: {$first} cannot be converted to {$second}.`);
+        }
     }    
 }
-
+?>
