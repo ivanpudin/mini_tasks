cc50692767c6fc81044e16acc41af88c82b7eda5
shree0007 <shree.bhusal07@gmail.com>
2023-04-09 22:39:42 +0300

    fixed round up and precision in temperature converter

--- src/task1/temperatureConvertor.php
+++ src/task1/temperatureConvertor.php
@@ -8,17 +8,17 @@ class temperatureConvertor
 
         switch (true) {
             case ($first == 'celsius' && $second == 'fahrenheit'):
-                return round ($amount * 9/5) + 32;
+                return (round ($amount * 9/5, 2) + 32);
             case ($first == 'celsius' && $second == 'kelvin'):
-                return round ($amount + 273.15);
+                return round (($amount) + 273.15, 2);
             case ($first == 'fahrenheit' && $second == 'celsius'):
-                return round ($amount - 32) * 5/9;
+                return round (($amount - 32) * 5/9, 2);
             case ($first == 'fahrenheit' && $second == 'kelvin'):
-                return round ($amount + 459.67) * 5/9;
+                return round (($amount + 459.67) * 5/9, 2);
             case ($first == 'kelvin' && $second == 'celsius'):
-                return round ($amount - 273.15);
+                return round (($amount) - 273.15, 2);
             case ($first == 'kelvin' && $second == 'fahrenheit'):
-                return round (($amount * 9/5) - 459.67);
+                return round(($amount * 9/5) - 459.67, 2);
             default:
                 die(`You have chosen the wrong units: {$first} cannot be converted to {$second}.`);
         }
