0436afc5323f3ec24cdb62b2098e2709f9728329
ivanpudin <ivanpudin9@gmail.com>
2023-04-11 12:31:38 +0300

    added task 2 manual and template

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <h1>Hello, camel_case team!</h1>
 
 <a href="./task1/index.php">Measurement conversion</a><br>
-<a href="./task2/index.php">Contact Us Form</a><br>
+<a href="./task2/webform.html">Contact Us Form</a><br>
 <a href="./task3/index.php">To-Do App using PHP and MySQL</a><br>
