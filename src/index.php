7d78f3e7e8dc04b478b0f2d4daf5e94a2df97742
ivanpudin <ivanpudin9@gmail.com>
2023-04-13 12:23:57 +0300

    styled the landing page

--- src/index.php
+++ src/index.php
@@ -1,5 +1,31 @@
-<h1>Hello, camel_case team!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>camel_case</title>
+    <link rel="stylesheet" href="./index.css" />
+</head>
 
-<a href="./task1/index.php">Measurement conversion</a><br>
-<a href="./task2/webform.php">Contact Us Form</a><br>
-<a href="./task3/index.php">To-Do App using PHP and MySQL</a><br>
+<body>
+    <a href="./task1/index.php">
+        <div class="container">
+            <img src="./content/placeholder1.jpg" alt="placeholder image">
+            <h1>Convert values</h1>
+        </div>
+    </a>
+    <a href="./task2/webform.php">
+        <div class="container">
+            <img src="./content/placeholder2.jpg" alt="placeholder image">
+            <h1>Contact us</h1>
+        </div>
+    </a>
+    <a href="./task3/index.php">
+        <div class="container">
+            <img src="./content/placeholder3.jpg" alt="placeholder image">
+            <h1>To do</h1>
+        </div>
+    </a>
+</body>
+</html>
--- src/task1/index.php
+++ src/task1/index.php
@@ -1,11 +1,22 @@
-<link rel="stylesheet" href="style.css" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Measurement convertor</title>
+    <link rel="stylesheet" href="./style.css" />
+</head>
+<body>
+    <header>
+        <h1>Measurement convertor</h1>
+        <a href="../index.php"> Back</a>
+    </header>
 
-<header>
-    <h1>Measurement convertor</h1>
-</header>
-
-<main>
-    <?php
+    <main>
+        <?php
 include 'measurement.php';
 ?>
-</main>
+    </main>
+</body>
+</html>
