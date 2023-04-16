897077fd67f7430bc0c2997bd167b502444f57b6
shree0007 <shree.bhusal07@gmail.com>
2023-04-16 13:50:47 +0300

    contact-form

---
+++ src/task2/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$host='Database';
+$user='root';
+$pass='lionPass';
+$dbname='contactformdb';
+
+
+ //check the MYSQL connection status
+ $conn=new mysqli($host,$user, $pass, $dbname);
+
+?>
--- src/task2/webform.php
+++ src/task2/webform.php
@@ -1,3 +1,27 @@
+<?php
+include 'connect.php';
+
+if(isset($_POST['submit'])){
+
+
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $subject= $_POST['subject'];
+    $message= $_POST['message'];
+    $q= "INSERT INTO `contact`(`name`, `email`,`subject`, `message`) VALUES ('$name', '$email', '$subject', '$message')";
+
+    $query = mysqli_query($conn, $q);
+
+
+     if($query){
+        echo "<script>alert('Thank you for contacting us');</script>";
+     }
+ 
+
+}
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,28 +34,31 @@
 
 <body>
     <div class="container">
+        <h1>Contact Us</h1>
         <form action="webform.php" method="POST" class="form">
             <div class="form-group">
                 <label for="name" class="form-label">Your Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Jane Doe" tabindex="1" required>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Please enter your name" tabindex="1" required>
             </div>
             <div class="form-group">
                 <label for="email" class="form-label">Your Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="jane@doe.com" tabindex="2" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Please enter your email" tabindex="2" required>
             </div>
             <div class="form-group">
                 <label for="subject" class="form-label">Subject</label>
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Hello There!" tabindex="3" required>
+                <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" tabindex="3" required>
             </div>
             <div class="form-group">
                 <label for="message" class="form-label">Message</label>
                 <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter Message..." tabindex="4"></textarea>
             </div>
             <div>
-                <button type="submit" class="btn">Send Message!</button>
+                <button type="submit" name="submit" class="btn">Send Message!</button>
             </div>
         </form>
     </div>
 </body>
 
 </html>
+
+<style><?php include 'webform.css'?></style>
